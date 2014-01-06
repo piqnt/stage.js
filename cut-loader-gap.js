2c165d3185692cb5a59a007afb4d9195443337b6
Ali Shakiba <ali@shakiba.me>
2014-01-06 08:06:56 +0330

    2014

--- cut-loader-gap.js
+++ cut-loader-gap.js
@@ -1,6 +1,6 @@
 /*
  * Cut.js
- * Copyright (c) 2013 Ali Shakiba, Piqnt LLC and other contributors
+ * Copyright (c) 2013-2014 Ali Shakiba, Piqnt LLC and other contributors
  * Available under the MIT license
  * @license
  */
