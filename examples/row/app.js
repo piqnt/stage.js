feadd0ae96a7eb67698532e125f4f37cd2f0e639
Ali Shakiba <ali@shakiba.me>
2014-01-16 17:35:44 +0330

    refactoring

--- examples/row/app.js
+++ examples/row/app.js
@@ -19,16 +19,15 @@ Cut.Loader.load(function(root, container) {
 
   var last = null;
   var row = Cut.row(0.5).appendTo(frame).pin("align", 0.5).spy(true).id("box");
-  for ( var i = 0; i < 6; i++) {
+  for (var i = 0; i < 6; i++) {
     var cell = Cut.image("base:color_" + colors[2 + i]).appendTo(row).id(i);
     cell.listen(Cut.Mouse.MOVE, function(ev, point) {
-      if (last == this) {
-        return;
+      if (last != this) {
+        last = this;
+        var color = colors[Math.floor(Math.random() * colors.length)];
+        this.setImage("base:color_" + color);
       }
-      last = this;
-      var color = colors[Math.floor(Math.random() * colors.length)];
-      this.setImage("base:color_" + color);
     });
   }
-  
+
 });
