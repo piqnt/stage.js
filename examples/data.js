c1b4d614a391e20a7f5d22d52869c754d6dd43e1
Ali Shakiba <ali@shakiba.me>
2014-01-20 01:33:47 +0330

    edit example page/data

---
+++ examples/data.js
@@ -0,0 +1,36 @@
+var examples = [ {
+  name : "Row Dynamic",
+  url : "row",
+  source : "https://github.com/piqnt/cut.js/tree/master/examples/row",
+  tags : "row tween"
+}, {
+  name : "Row Static",
+  url : "tween",
+  source : "https://github.com/piqnt/cut.js/tree/master/examples/tween",
+  tags : "row tween"
+}, {
+  name : "Column x Row",
+  url : "grid",
+  source : "https://github.com/piqnt/cut.js/tree/master/examples/grid",
+  tags : "row column tween"
+}, {
+  name : "Number",
+  url : "box",
+  source : "https://github.com/piqnt/cut.js/tree/master/examples/box",
+  tags : "string image box"
+}, {
+  name : "Tile",
+  url : "tile",
+  source : "https://github.com/piqnt/cut.js/tree/master/examples/tile",
+  tags : "image tween"
+}, {
+  name : "Stretch",
+  url : "stretch",
+  source : "https://github.com/piqnt/cut.js/tree/master/examples/stretch",
+  tags : "image tween"
+}, {
+  name : "Bars",
+  url : "bars",
+  source : "https://github.com/piqnt/cut.js/tree/master/examples/bars",
+  tags : "image tween"
+} ];
