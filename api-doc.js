52958e2cc3775d46a09e4890d31815c47551354b
Ali Shakiba <ali@shakiba.me>
2014-02-10 01:11:32 +0330

    update api-doc

--- api-doc.js
+++ api-doc.js
@@ -1,7 +1,8 @@
 //
 // ### Cut
-//
-// Create a new plain cut instance.
+// The tree nodes are called cut.
+
+// Create new plain cut instance.
 // No painting is associated with a plain cut, it is just a parent for other cuts.
 var foo = Cut.create();
 
@@ -48,6 +49,41 @@ bar.visible(visible);
 bar.hide();
 bar.show();
 
+// Register a ticker to be called on ticking (before painting).
+// It can be used to modify the cut.
+foo.tick(ticker, beforeChildren = false);
+
+// Register a type-listener to bar.
+// `type` can be array or spaced string of multiple values.
+foo.on(type, listener);
+
+// Get type-listeners registered to bar.
+foo.listeners(type);
+
+// Call type-listeners with args.
+foo.publish(type, args);
+
+// Visit foo's sub-tree.
+foo.visit({
+  start : function() {
+    return skipChildren ? true : false;
+  },
+  end : function() {
+    return stopVisit ? true : false;
+  },
+  reverse : reverseChildrenOrder ? true : false,
+  visible : onlyVisibleCuts ? true : false
+});
+
+// Rendering pauses unless/until at least one cut is touched directly or
+// indirectly.
+foo.touch();
+
+//
+// ### Pinning
+// Pinnint is a top level concept, it refers to transforming a node/cut relative
+// to its parent.
+
 // Get a pinning value.
 bar.pin(name);
 // Set a pinning value.
@@ -92,68 +128,39 @@ bar.pin({
   resizeHeight : "",
 });
 
-// Register a ticker to be called on ticking (before painting).
-// It can be used to modify the cut.
-foo.tick(ticker, beforeChildren = false);
-
-// Register a type-listener to bar.
-// `type` can be array or spaced string of multiple values.
-foo.on(type, listener);
-
-// Get type-listeners registered to bar.
-foo.listeners(type);
-
-// Call type-listeners with args.
-foo.publish(type, args);
-
-// Visit foo's sub-tree.
-foo.visit({
-  start : function() {
-    return skipChildren ? true : false;
-  },
-  end : function() {
-    return stopVisit ? true : false;
-  },
-  reverse : reverseChildrenOrder ? true : false,
-  visible : onlyVisibleCuts ? true : false
-});
-
-// Rendering pauses unless/until at least one cut is touched directly or
-// indirectly.
-foo.touch();
-
 //
 // ### Tweening
-//
+// Tweening can only be applied to pinning values.
 
-// create a tweening entry
+// Create a tweening entry.
 var tween = foo.tween(duration = 400, delay = 0);
 
-// clear tweening queue
+// Clear tweening queue.
 tween.clear(jumpToEnd = false);
 
-// set pinning values and start tweening
+// Set pinning values and start tweening. Currently short-hand names are not
+// supported for tweening.
 tween.pin(pinning);
 
 //
 // ### Row
-//
-// Create a new row.
 // A row is a cut which organizes its children as a horizontal sequence.
+
+// Create a new row.
 var row = Cut.row(valign = 0);
 
 //
 // ### Column
-//
-// Create a new column.
 // A column is a cut which organizes its children as a vertical sequence.
+
+// Create a new column.
 var column = Cut.column(halign = 0);
 
 //
-// ### Cut
-//
-// Create a new image instance.
+// ### Image
 // An image is a cut which pastes a cutout.
+
+// Create a new image instance.
 var image = Cut.image(cutout);
 
 // Change image.
@@ -173,9 +180,9 @@ image.stretch();
 
 //
 // ### Anim(Clip)
-//
-// Create a new anim instance.
 // An anim is a cut which have a set of cutouts and pastes a cutout at a time.
+
+// Create a new anim instance.
 var anim = Cut.anim(cutouts, fps = Cut.Anim.FPS);
 
 // Get or set anim fps.
@@ -201,9 +208,9 @@ anim.repeat(repeat, callback = null);
 
 //
 // ### String
-//
+// String is a row of image cuts.
+
 // Create a new string (sequence) instance.
-// String is a row of anim cuts.
 Cut.string(cutouts);
 
 string.setFont(cutouts);
@@ -213,7 +220,6 @@ string.setValue(value);
 
 //
 // ### Cutout
-//
 // There are two ways to define a cutout (sprite): Canvas drawing and image
 // textures.
 
@@ -227,7 +233,6 @@ cutout = Cut.Out.drawing(name = "random", width, height, ratio = 1, function(
 
 //
 // ### Textures
-//
 // Register a texture, images are automatically loaded by Cut.Loader.
 Cut.addTexture({
   name : "",
@@ -241,7 +246,7 @@ Cut.addTexture({
     y : "",
     width : "",
     height : "",
-    // Optional, used by tile:
+    // Optional, used by tiled and stretched images.
     top : "",
     bottom : "",
     left : "",
@@ -257,7 +262,8 @@ cutouts = "textureName:cutoutPrefix";
 
 //
 // ### Loader
-//
+// Loaders are used to start and run apps.
+
 // Load an app with root node and container element.
 Cut.Loader.load(function(root, container) {
   // add cuts to root
@@ -266,9 +272,10 @@ Cut.Loader.load(function(root, container) {
 
 //
 // ### Mouse(Touch)
-//
-// Subscribe the root to mouse/touch events.
-Cut.Mouse.subscribe(root, container, includingMoveEvents = false);
+// Mouse class is used to capture and process mouse and touch events.  
+
+// Subscribe root.
+Cut.Mouse.subscribe(root, container, captureAnyMove = false);
 
 // Add click listener to bar, other mouse/touch event types are start, end and
 // move.
@@ -279,6 +286,7 @@ bar.on(Cut.Mouse.CLICK, function(event, point) {
 //
 // ### Extending Cut
 //
+
 // Home extends Cut.
 function Home() {
   Home.prototype._super.apply(this, arguments);
