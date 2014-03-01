258e9095671b3dac74871c4914f306ccd7b9c30d
Ali Shakiba <ali@shakiba.me>
2014-03-01 23:10:15 +0330

    +pinwheel

---
+++ examples/basic-pinwheel/app.js
@@ -0,0 +1,37 @@
+Cut.Loader.load(function(root, container) {
+  Cut.Mouse.subscribe(root, container, true);
+  root.viewbox(1000, 1000).pin("handle", -0.5);
+
+  var clicked = false;
+  var logo = Cut.image("pin:wheel").appendTo(root).pin("pivot", 0.5).pin(
+      "handle", 0.5).on(Cut.Mouse.CLICK, function() {
+    clicked = true;
+    then();
+  });
+
+  function then() {
+    logo.pin("rotation", logo.pin("rotation") % (Math.PI * 2));
+    if (clicked) {
+      logo.tween(1000).clear().pin("rotation",
+          logo.pin("rotation") - Math.PI * 2).then(then);
+    } else {
+      logo.tween(2000).clear().pin("rotation",
+          logo.pin("rotation") - Math.PI * 2).easing(function(t) {
+        return Math.sin(t * Math.PI / 2);
+      });
+    }
+    clicked = false;
+  }
+});
+
+Cut.addTexture({
+  name : "pin",
+  imagePath : "pinwheel.png",
+  cutouts : [ {
+    name : "wheel",
+    x : 0,
+    y : 0,
+    width : 200,
+    height : 200
+  } ]
+});
