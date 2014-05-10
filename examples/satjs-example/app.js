2534fcfad1b0b9ede9d876012cf668f4aa9548c9
Ali Shakiba <ali@shakiba.me>
2014-05-10 16:02:28 +0430

    update satjs example

--- examples/satjs-example/app.js
+++ examples/satjs-example/app.js
@@ -1,31 +1,41 @@
-var V = function(x, y) {
-  return new SAT.Vector(x, y);
-};
-
 Cut(function(root, container) {
   Cut.Mouse(root, container);
   root.viewbox(400, 400).pin("handle", -0.5);
 
   var world = new SAT.World();
-  var poly = new SAT.Body(new SAT.Polygon(V(-60, -60), [ V(0, -30), V(30, 0),
-      V(0, 30), V(-30, 0) ]), {
+
+  var poly = new SAT.Body(P(V(60, 0), [ V(0, -30), V(30, 0), V(0, 30),
+      V(-30, 0) ]), {
     solid : true
   });
   world.addBody(poly);
-  world.addBody(new SAT.Body(new SAT.Polygon(V(60, 60), [ V(-20, -20),
-      V(20, -20), V(20, 20), V(-20, 20) ]), {
-    solid : true
-  }));
-  world.addBody(new SAT.Body(new SAT.Circle(V(0, 0), 30), {
+
+  world.addBody(new SAT.Body(C(V(-60, 0), 25), {
     solid : true,
     heavy : true
   }));
 
-  var time = 0;
+  world.addBody(new SAT.Body(P(V(0, 0), [ V(-20, -20), V(20, -20), V(20, 20),
+      V(-20, 20) ]), {
+    solid : true
+  }));
+
   root.tick(function(t) {
-    poly.shape.setAngle(poly.shape.angle + (Math.PI / 60));
+    poly.shape.setAngle(poly.shape.angle + (Math.PI * t / 1000));
     root.touch();
   });
 
   new Cut.SAT(world).appendTo(root);
 });
+
+function V(x, y) {
+  return new SAT.Vector(x, y);
+};
+
+function P(c, vs) {
+  return new SAT.Polygon(c, vs);
+};
+
+function C(c, r) {
+  return new SAT.Circle(c, r);
+};
