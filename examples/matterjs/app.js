ca108ee539182c18ea8edca9f500613d7ae737c3
Ali Shakiba <ali@shakiba.me>
2014-05-16 00:21:49 +0430

    +matterjs

---
+++ examples/matterjs/app.js
@@ -0,0 +1,102 @@
+var ppu = 16.0;
+
+Cut(function(root, container) {
+  Cut.Mouse(root, container, true);
+
+  var M = Cut.Math, Mouse = Cut.Mouse;
+  var Engine = Matter.Engine, World = Matter.World, Bodies = Matter.Bodies, Body = Matter.Body, Events = Matter.Events;
+
+  root.MAX_ELAPSE = 100;
+
+  var width = 600, height = 600, corner = 75;
+
+  var options = {
+    world : {
+      gravity : {
+        x : 0,
+        y : 0
+      },
+      bounds : {
+        min : {
+          x : -Infinity,
+          y : -Infinity
+        },
+        max : {
+          x : Infinity,
+          y : Infinity
+        }
+      }
+    },
+    render : {
+      controller : {
+        create : function(options) {
+          return options;
+        },
+        clear : function() {
+        },
+        world : function(engine) {
+        }
+      }
+    },
+    positionIterations : 6,
+    velocityIterations : 4,
+    enableSleeping : true
+  };
+
+  var engine = Engine.create(options);
+  var world = engine.world;
+
+  var frame = {
+    isStatic : true,
+    restitution : 1
+  };
+  World.add(world, Bodies.rectangle(width / 2, 0, width, 50, frame));
+  World.add(world, Bodies.rectangle(width / 2, height, width, 50, frame));
+  World.add(world, Bodies.rectangle(0, height / 2, 50, height, frame));
+  World.add(world, Bodies.rectangle(width, height / 2, 50, height, frame));
+  World.add(world, Bodies.rectangle(0, 0, corner, corner, frame));
+  World.add(world, Bodies.rectangle(0, height, corner, corner, frame));
+  World.add(world, Bodies.rectangle(width, 0, corner, corner, frame));
+  World.add(world, Bodies.rectangle(width, height, corner, corner, frame));
+
+  var blockId = Body.nextGroupId();
+  for (var i = 0; i <= 10; i++) {
+    for (var j = 0; j <= 10; j++) {
+      World.add(world, Bodies.rectangle(100 + i * 40, 100 + j * 40, 40, 40, {
+        groupId : blockId,
+        isStatic : true
+      }));
+    }
+  }
+
+  var ball = Bodies.circle(100, 100, 20, {
+    frictionAir : 0,
+    friction : 0,
+    restitution : 1,
+    inertia : Infinity,
+    mass : 1
+  });
+  World.add(world, ball);
+
+  var a = Math.random() * Math.PI * 2;
+  Body.applyForce(ball, {
+    x : 0,
+    y : 0
+  }, {
+    x : 0.05 * Math.cos(a),
+    y : 0.05 * Math.sin(a)
+  });
+
+  Events.on(engine, 'collisionEnd', function(event) {
+    var pairs = event.pairs;
+    for (var i = 0; i < pairs.length; i++) {
+      var pair = pairs[i];
+      var a = pair.bodyA, b = pair.bodyB;
+      var block = (a.groupId == blockId) && a || (b.groupId == blockId) && b;
+      block && World.remove(world, block);
+    }
+  });
+
+  new Cut.Matter(engine).appendTo(root);
+
+});
---
+++ examples/matterjs/cut-matter.js
@@ -0,0 +1,166 @@
+/**
+ * CutJS viewer for Matter.js
+ */
+Cut.Matter = function(engine) {
+  Cut.Matter.prototype._super.apply(this, arguments);
+
+  var Engine = Matter.Engine, Composite = Matter.Composite, Events = Matter.Events;
+
+  var self = this;
+  var world = this.world = engine.world;
+
+  Events.on(world, 'addBody', function(ev) {
+    self.addRenderable(ev.body);
+  });
+
+  Events.on(world, 'removeBody', function(ev) {
+    self.removeRenderable(ev.body);
+  });
+
+  Events.on(engine, 'afterTick', function(ev) {
+    var bodies = Composite.allBodies(world);
+    for (var i = 0; i < bodies.length; i++) {
+      self.updateRenderable(bodies[i]);
+    }
+  });
+
+  var bodies = Composite.allBodies(world);
+  for (var i = 0; i < bodies.length; i++) {
+    self.addRenderable(bodies[i]);
+  }
+
+  var runner = Engine.runner(engine);
+  var time = 0;
+  this.tick(function(t) {
+    time += t;
+    runner(time);
+    this.touch();
+  });
+
+};
+
+Cut.Matter.prototype = Cut._create(Cut.prototype);
+Cut.Matter.prototype._super = Cut;
+Cut.Matter.prototype.constructor = Cut.Matter;
+
+Cut.Matter.prototype.addRenderable = function(body) {
+  if (!body.ui) {
+    var cutout;
+    if (body.circleRadius) {
+      cutout = this.drawCircle(body.circleRadius, body.render);
+    } else {
+      cutout = this.drawConvex(body.vertices, body.position, body.render);
+    }
+    if (cutout) {
+      body.ui = Cut.image(cutout).pin('handle', 0.5).appendTo(this);
+    }
+  }
+};
+
+Cut.Matter.prototype.removeRenderable = function(body) {
+  if (body.ui) {
+    body.ui.remove();
+  }
+};
+
+Cut.Matter.prototype.updateRenderable = function(body) {
+  if (body.ui) {
+    body.ui.pin('offsetX', body.position.x);
+    body.ui.pin('offsetY', body.position.y);
+    body.ui.pin('rotation', body.angle);
+  }
+};
+
+Cut.Matter.prototype.drawCircle = function(radius, options) {
+  var lineWidth = options.lineWidth;
+  var width = radius * 2 + lineWidth * 2;
+  var height = radius * 2 + lineWidth * 2;
+
+  return Cut.Out.drawing(width, height, function(ctx, ratio) {
+    ctx.scale(ratio, ratio);
+    ctx.beginPath();
+    ctx.arc(width / 2, height / 2, radius, 0, 2 * Math.PI);
+    if (options.fillStyle) {
+      ctx.fillStyle = options.fillStyle;
+      ctx.fill();
+    }
+    ctx.lineWidth = options.lineWidth;
+    ctx.strokeStyle = options.strokeStyle;
+    ctx.stroke();
+  });
+};
+
+Cut.Matter.prototype.drawConvex = function(verts, base, options) {
+  var lineWidth = options.lineWidth;
+
+  if (!verts.length) {
+    return;
+  }
+
+  var x0 = base.x, y0 = base.y;
+
+  var width = 0, height = 0;
+  for (var i = 0; i < verts.length; i++) {
+    var v = verts[i], x = v.x, y = v.y;
+    width = Math.max(Math.abs(x - x0), width);
+    height = Math.max(Math.abs(y - y0), height);
+  }
+
+  var cutout = Cut.Out.drawing(2 * width + 2 * lineWidth, 2 * height + 2
+      * lineWidth, function(ctx, ratio) {
+    ctx.scale(ratio, ratio);
+
+    ctx.beginPath();
+    for (var i = 0; i < verts.length; i++) {
+      var v = verts[i], x = v.x - x0 + width + lineWidth, y = v.y - y0 + height
+          + lineWidth;
+      if (i == 0)
+        ctx.moveTo(x, y);
+      else
+        ctx.lineTo(x, y);
+    }
+
+    if (verts.length > 2) {
+      ctx.closePath();
+    }
+
+    if (options.fillStyle) {
+      ctx.fillStyle = options.fillStyle;
+      ctx.fill();
+      ctx.closePath();
+    }
+
+    ctx.lineCap = "round";
+    ctx.lineWidth = options.lineWidth;
+    ctx.strokeStyle = options.strokeStyle;
+    ctx.stroke();
+  });
+
+  return cutout;
+};
+
+(function() {
+  var origin = Matter.Composite.addBody;
+  Matter.Composite.addBody = function(composite, body) {
+    var world = composite;
+    while (world.parent)
+      world = world.parent;
+    Matter.Events.trigger(world, "addBody", {
+      body : body
+    });
+    return origin.apply(this, arguments);
+  };
+})();
+
+(function() {
+  var origin = Matter.Composite.removeBody;
+  Matter.Composite.removeBody = function(composite, body) {
+    var world = composite;
+    while (world.parent)
+      world = world.parent;
+    Matter.Events.trigger(world, "removeBody", {
+      body : body
+    });
+    return origin.apply(this, arguments);
+  };
+})();
