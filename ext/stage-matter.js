9a73711be313572855d202355da5f4f199952620
Ali Shakiba <ali@shakiba.me>
2015-06-26 11:18:29 +0430

    umd for exts

--- ext/stage-matter.js
+++ ext/stage-matter.js
@@ -1,200 +1,216 @@
 /**
- * Matter.js viewer for Stage.js
+ * Matter.js viewer
  */
-Stage.Matter = function(engine) {
-  Stage.Matter._super.call(this);
+(function() {
 
-  var Engine = Matter.Engine;
   var Composite = Matter.Composite;
   var Events = Matter.Events;
-  var Runner = Matter.Runner || Engine;
+  var Runner = Matter.Runner || Matter.Engine;
 
-  engine.render = {
-    controller : {
-      create : function(options) {
-        return options;
-      },
-      clear : function() {
-      },
-      world : function(engine) {
-      },
-      setBackground : function() {
+  Viewer._super = Stage;
+  Viewer.prototype = Stage._create(Viewer._super.prototype);
+
+  function Viewer(engine) {
+    Viewer._super.call(this);
+
+    engine.render = {
+      controller : {
+        create : function(options) {
+          return options;
+        },
+        clear : function() {
+        },
+        world : function(engine) {
+        },
+        setBackground : function() {
+        }
       }
-    }
-  };
-  engine.input = {
-    mouse : {
-      sourceEvents : {}
-    }
-  };
+    };
+    engine.input = {
+      mouse : {
+        sourceEvents : {}
+      }
+    };
 
-  var self = this;
-  var world = this.world = engine.world;
+    var self = this;
+    var world = this.world = engine.world;
 
-  Events.on(world, 'addBody', function(ev) {
-    self.addRenderable(ev.body);
-  });
+    Events.on(world, 'addBody', function(ev) {
+      self.addRenderable(ev.body);
+    });
 
-  Events.on(world, 'removeBody', function(ev) {
-    self.removeRenderable(ev.body);
-  });
+    Events.on(world, 'removeBody', function(ev) {
+      self.removeRenderable(ev.body);
+    });
 
-  Events.on(engine, 'afterTick', function(ev) {
+    Events.on(engine, 'afterTick', function(ev) {
+      var bodies = Composite.allBodies(world);
+      for (var i = 0; i < bodies.length; i++) {
+        if (!bodies[i].isSleeping) {
+          self.updateRenderable(bodies[i]);
+        }
+      }
+    });
+
     var bodies = Composite.allBodies(world);
     for (var i = 0; i < bodies.length; i++) {
-      if (!bodies[i].isSleeping) {
-        self.updateRenderable(bodies[i]);
+      self.addRenderable(bodies[i]);
+    }
+
+    var runner = Runner.runner(engine);
+    var time = 0;
+    this.tick(function(t) {
+      time += t;
+      runner(time);
+      return true;
+    });
+  }
+
+  Viewer.prototype.addRenderable = function(body) {
+    if (!body.ui) {
+      var texture = null;
+      if (body.circleRadius) {
+        texture = this.drawCircle(body.circleRadius, body.render);
+      } else if (body.vertices) {
+        texture = this.drawConvex(body.vertices, body.position, body.render);
       }
+      if (texture) {
+        body.ui = Stage.image(texture).pin('handle', 0.5).appendTo(this);
+      }
+      this.updateRenderable(body);
     }
-  });
+  };
 
-  var bodies = Composite.allBodies(world);
-  for (var i = 0; i < bodies.length; i++) {
-    self.addRenderable(bodies[i]);
-  }
-
-  var runner = Runner.runner(engine);
-  var time = 0;
-  this.tick(function(t) {
-    time += t;
-    runner(time);
-    return true;
-  });
-};
-
-Stage.Matter._super = Stage;
-Stage.Matter.prototype = Stage._create(Stage.Matter._super.prototype);
-Stage.Matter.prototype.constructor = Stage.Matter;
-
-Stage.Matter.prototype.addRenderable = function(body) {
-  if (!body.ui) {
-    var texture = null;
-    if (body.circleRadius) {
-      texture = this.drawCircle(body.circleRadius, body.render);
-    } else if (body.vertices) {
-      texture = this.drawConvex(body.vertices, body.position, body.render);
-    }
-    if (texture) {
-      body.ui = Stage.image(texture).pin('handle', 0.5).appendTo(this);
-    }
-    this.updateRenderable(body);
-  }
-};
-
-Stage.Matter.prototype.removeRenderable = function(body) {
-  if (body.ui) {
-    body.ui.remove();
-  }
-};
-
-Stage.Matter.prototype.updateRenderable = (function() {
-  var pin = {};
-  return function(body) {
+  Viewer.prototype.removeRenderable = function(body) {
     if (body.ui) {
-      pin.offsetX = body.position.x;
-      pin.offsetY = body.position.y;
-      pin.rotation = body.angle;
-      body.ui.pin(pin);
+      body.ui.remove();
     }
   };
-})();
 
-Stage.Matter.prototype.drawCircle = function(radius, options) {
-  var lineWidth = options.lineWidth;
+  Viewer.prototype.updateRenderable = (function() {
+    var pin = {};
+    return function(body) {
+      if (body.ui) {
+        pin.offsetX = body.position.x;
+        pin.offsetY = body.position.y;
+        pin.rotation = body.angle;
+        body.ui.pin(pin);
+      }
+    };
+  })();
 
-  var width = radius * 2 + lineWidth * 2;
-  var height = radius * 2 + lineWidth * 2;
-  var ratio = 1;
+  Viewer.prototype.drawCircle = function(radius, options) {
+    var lineWidth = options.lineWidth;
 
-  return Stage.canvas(function(ctx) {
+    var width = radius * 2 + lineWidth * 2;
+    var height = radius * 2 + lineWidth * 2;
+    var ratio = 1;
 
-    this.size(width, height, ratio);
+    return Stage.canvas(function(ctx) {
 
-    ctx.scale(ratio, ratio);
-    ctx.arc(width / 2, height / 2, radius, 0, 2 * Math.PI);
-    if (options.fillStyle) {
-      ctx.fillStyle = options.fillStyle;
-      ctx.fill();
+      this.size(width, height, ratio);
+
+      ctx.scale(ratio, ratio);
+      ctx.arc(width / 2, height / 2, radius, 0, 2 * Math.PI);
+      if (options.fillStyle) {
+        ctx.fillStyle = options.fillStyle;
+        ctx.fill();
+      }
+      ctx.lineTo(width / 2, height / 2);
+      ctx.lineWidth = options.lineWidth;
+      ctx.strokeStyle = options.strokeStyle;
+      ctx.stroke();
+    });
+  };
+
+  Viewer.prototype.drawConvex = function(verts, base, options) {
+    var lineWidth = options.lineWidth;
+
+    if (!verts.length) {
+      return;
     }
-    ctx.lineTo(width/2, height/2);
-    ctx.lineWidth = options.lineWidth;
-    ctx.strokeStyle = options.strokeStyle;
-    ctx.stroke();
-  });
-};
 
-Stage.Matter.prototype.drawConvex = function(verts, base, options) {
-  var lineWidth = options.lineWidth;
+    var x0 = base.x, y0 = base.y;
 
-  if (!verts.length) {
-    return;
-  }
+    var width = 0, height = 0;
+    var ratio = 1;
 
-  var x0 = base.x, y0 = base.y;
-
-  var width = 0, height = 0;
-  var ratio = 1;
-
-  for (var i = 0; i < verts.length; i++) {
-    var v = verts[i], x = v.x, y = v.y;
-    width = Math.max(Math.abs(x - x0), width);
-    height = Math.max(Math.abs(y - y0), height);
-  }
-
-  return Stage.canvas(function(ctx) {
-
-    this.size(2 * width + 2 * lineWidth, 2 * height + 2 * lineWidth, ratio);
-
-    ctx.scale(ratio, ratio);
-    ctx.beginPath();
     for (var i = 0; i < verts.length; i++) {
-      var v = verts[i], x = v.x - x0 + width + lineWidth, y = v.y - y0 + height
-          + lineWidth;
-      if (i == 0)
-        ctx.moveTo(x, y);
-      else
-        ctx.lineTo(x, y);
+      var v = verts[i], x = v.x, y = v.y;
+      width = Math.max(Math.abs(x - x0), width);
+      height = Math.max(Math.abs(y - y0), height);
     }
 
-    if (verts.length > 2) {
-      ctx.closePath();
-    }
+    return Stage.canvas(function(ctx) {
 
-    if (options.fillStyle) {
-      ctx.fillStyle = options.fillStyle;
-      ctx.fill();
-      ctx.closePath();
-    }
+      this.size(2 * width + 2 * lineWidth, 2 * height + 2 * lineWidth, ratio);
 
-    ctx.lineCap = "round";
-    ctx.lineWidth = options.lineWidth;
-    ctx.strokeStyle = options.strokeStyle;
-    ctx.stroke();
-  });
-};
+      ctx.scale(ratio, ratio);
+      ctx.beginPath();
+      for (var i = 0; i < verts.length; i++) {
+        var v = verts[i], x = v.x - x0 + width + lineWidth, y = v.y - y0
+            + height + lineWidth;
+        if (i == 0)
+          ctx.moveTo(x, y);
+        else
+          ctx.lineTo(x, y);
+      }
 
-(function() {
-  var origin = Matter.Composite.addBody;
-  Matter.Composite.addBody = function(composite, body) {
-    var world = composite;
-    while (world.parent)
-      world = world.parent;
-    Matter.Events.trigger(world, "addBody", {
-      body : body
+      if (verts.length > 2) {
+        ctx.closePath();
+      }
+
+      if (options.fillStyle) {
+        ctx.fillStyle = options.fillStyle;
+        ctx.fill();
+        ctx.closePath();
+      }
+
+      ctx.lineCap = "round";
+      ctx.lineWidth = options.lineWidth;
+      ctx.strokeStyle = options.strokeStyle;
+      ctx.stroke();
     });
-    return origin.apply(this, arguments);
   };
-})();
 
-(function() {
-  var origin = Matter.Composite.removeBody;
-  Matter.Composite.removeBody = function(composite, body) {
-    var world = composite;
-    while (world.parent)
-      world = world.parent;
-    Matter.Events.trigger(world, "removeBody", {
-      body : body
+  (function() {
+    var origin = Composite.addBody;
+    Composite.addBody = function(composite, body) {
+      var world = composite;
+      while (world.parent)
+        world = world.parent;
+      Events.trigger(world, "addBody", {
+        body : body
+      });
+      return origin.apply(this, arguments);
+    };
+  })();
+
+  (function() {
+    var origin = Composite.removeBody;
+    Composite.removeBody = function(composite, body) {
+      var world = composite;
+      while (world.parent)
+        world = world.parent;
+      Events.trigger(world, "removeBody", {
+        body : body
+      });
+      return origin.apply(this, arguments);
+    };
+  })();
+
+  if (typeof module !== 'undefined') {
+    module.exports = Viewer;
+  } else if (typeof define === 'function' && define.amd) {
+    define([], function() {
+      return Viewer;
     });
-    return origin.apply(this, arguments);
-  };
-})();
+  } else if (typeof Stage !== 'undefined') {
+    Stage.Matter = Viewer;
+  } else if (typeof window !== 'undefined') {
+  } else if (typeof global !== 'undefined') {
+  } else if (typeof self !== 'undefined') {
+  } else {
+  }
+
+}).call(this);
--- ext/stage-sat.js
+++ ext/stage-sat.js
@@ -1,191 +1,208 @@
 /**
- * SAT.js viewer for Stage.js
+ * SAT.js viewer
  */
-Stage.SAT = function(world, options) {
-  Stage.SAT._super.call(this);
+(function() {
 
-  var self = this;
-  this.world = world;
+  Viewer._super = Stage;
+  Viewer.prototype = Stage._create(Viewer._super.prototype);
 
-  this.options = {
-    lineWidth : 2,
-    lineColor : '#000000',
-    fillColor : function() {
-      var red = Stage.Math.random(192, 256) | 0;
-      var green = Stage.Math.random(192, 256) | 0;
-      var blue = Stage.Math.random(192, 256) | 0;
-      return "#" + red.toString(16) + green.toString(16) + blue.toString(16);
-    },
-    ratio : 2,
-    get : function(key) {
-      var value = this[key];
-      return typeof value === 'function' ? value() : value;
-    },
-    extend : function(options) {
-      return Stage._extend({}, this, options);
+  function Viewer(world, options) {
+    Viewer._super.call(this);
+
+    var self = this;
+    this.world = world;
+
+    this.options = {
+      lineWidth : 2,
+      lineColor : '#000000',
+      fillColor : function() {
+        var red = Stage.Math.random(192, 256) | 0;
+        var green = Stage.Math.random(192, 256) | 0;
+        var blue = Stage.Math.random(192, 256) | 0;
+        return "#" + red.toString(16) + green.toString(16) + blue.toString(16);
+      },
+      ratio : 2,
+      get : function(key) {
+        var value = this[key];
+        return typeof value === 'function' ? value() : value;
+      },
+      extend : function(options) {
+        return Stage._extend({}, this, options);
+      }
+    }.extend(options);
+
+    world.onAddBody = function(e) {
+      self.addRenderable(e.body);
+    };
+    world.onRemoveBody = function(e) {
+      self.removeRenderable(e.body);
+    };
+
+    // Add initial bodies
+    for (var i = 0; i < world.bodies.length; i++) {
+      this.addRenderable(world.bodies[i]);
     }
-  }.extend(options);
 
-  world.onAddBody = function(e) {
-    self.addRenderable(e.body);
-  };
-  world.onRemoveBody = function(e) {
-    self.removeRenderable(e.body);
-  };
+    this.tick(function(t) {
+      this.simulate(t);
+    });
 
-  // Add initial bodies
-  for (var i = 0; i < world.bodies.length; i++) {
-    this.addRenderable(world.bodies[i]);
+    var dragPoint = {}, dragShape = null;
+    this.attr('spy', true).on(Stage.Mouse.START, function(point) {
+      dragPoint = {
+        x : point.x,
+        y : point.y
+      };
+      dragShape = null;
+      for (var i = 0; i < this.world.bodies.length; i++) {
+        var body = this.world.bodies[i];
+        if (SAT.pointIn(point, body.shape)) {
+          dragShape = body.shape;
+          break;
+        }
+      }
+
+    }).on(Stage.Mouse.MOVE, function(point) {
+      if (dragShape) {
+        dragShape.pos.x -= dragPoint.x - point.x;
+        dragShape.pos.y -= dragPoint.y - point.y;
+      }
+      dragPoint = {
+        x : point.x,
+        y : point.y
+      };
+
+    }).on(Stage.Mouse.END, function(point) {
+      dragShape = null;
+    });
   }
 
-  this.tick(function(t) {
-    this.simulate(t);
-  });
+  Viewer.prototype.simulate = function(t) {
+    this.world.simulate();
 
-  var dragPoint = {}, dragShape = null;
-  this.attr('spy', true).on(Stage.Mouse.START, function(point) {
-    dragPoint = {
-      x : point.x,
-      y : point.y
-    };
-    dragShape = null;
     for (var i = 0; i < this.world.bodies.length; i++) {
       var body = this.world.bodies[i];
-      if (SAT.pointIn(point, body.shape)) {
-        dragShape = body.shape;
-        break;
+      body.ui && body.ui.pin({
+        offsetX : body.shape.pos.x,
+        offsetY : body.shape.pos.y,
+        rotation : body.shape.angle
+      });
+    }
+  };
+
+  Viewer.prototype.addRenderable = function(obj) {
+
+    obj.ui = Stage.create().appendTo(this);
+
+    var texture = null;
+    var shape = obj.shape;
+
+    if (shape instanceof SAT.Circle) {
+      texture = this.drawCircle(shape.r);
+
+    } else {
+      if (shape.points.length) {
+        texture = this.drawConvex(shape.points);
       }
     }
 
-  }).on(Stage.Mouse.MOVE, function(point) {
-    if (dragShape) {
-      dragShape.pos.x -= dragPoint.x - point.x;
-      dragShape.pos.y -= dragPoint.y - point.y;
-    }
-    dragPoint = {
-      x : point.x,
-      y : point.y
-    };
-
-  }).on(Stage.Mouse.END, function(point) {
-    dragShape = null;
-  });
-};
-
-Stage.SAT._super = Stage;
-Stage.SAT.prototype = Stage._create(Stage.SAT._super.prototype);
-Stage.SAT.prototype.constructor = Stage.SAT;
-
-Stage.SAT.prototype.simulate = function(t) {
-  this.world.simulate();
-
-  for (var i = 0; i < this.world.bodies.length; i++) {
-    var body = this.world.bodies[i];
-    body.ui && body.ui.pin({
-      offsetX : body.shape.pos.x,
-      offsetY : body.shape.pos.y,
-      rotation : body.shape.angle
+    Stage.image(texture).appendTo(obj.ui).pin({
+      handle : 0.5
     });
-  }
-};
 
-Stage.SAT.prototype.addRenderable = function(obj) {
+  };
 
-  obj.ui = Stage.create().appendTo(this);
+  Viewer.prototype.removeRenderable = function(obj) {
+    obj.ui && obj.ui.remove();
+  };
 
-  var texture = null;
-  var shape = obj.shape;
+  Viewer.prototype.drawCircle = function(radius, options) {
+    options = this.options.extend(options);
+    var lineWidth = options.get('lineWidth'), lineColor = options
+        .get('lineColor'), fillColor = options.get('fillColor');
 
-  if (shape instanceof SAT.Circle) {
-    texture = this.drawCircle(shape.r);
+    var width = radius * 2 + lineWidth * 2;
+    var height = radius * 2 + lineWidth * 2;
+    var ratio = options.ratio;
 
-  } else {
-    if (shape.points.length) {
-      texture = this.drawConvex(shape.points);
-    }
-  }
+    return Stage.canvas(function(ctx) {
+      this.size(width, height, ratio);
 
-  Stage.image(texture).appendTo(obj.ui).pin({
-    handle : 0.5
-  });
+      ctx.scale(ratio, ratio);
+      ctx.beginPath();
+      ctx.arc(width / 2, height / 2, radius, 0, 2 * Math.PI);
+      if (fillColor) {
+        ctx.fillStyle = fillColor;
+        ctx.fill();
+      }
 
-};
+      ctx.lineWidth = lineWidth;
+      ctx.strokeStyle = lineColor;
+      ctx.stroke();
+    });
+  };
 
-Stage.SAT.prototype.removeRenderable = function(obj) {
-  obj.ui && obj.ui.remove();
-};
+  Viewer.prototype.drawConvex = function(verts, options) {
+    options = this.options.extend(options);
+    var lineWidth = options.get('lineWidth'), lineColor = options
+        .get('lineColor'), fillColor = options.get('fillColor');
 
-Stage.SAT.prototype.drawCircle = function(radius, options) {
-  options = this.options.extend(options);
-  var lineWidth = options.get('lineWidth'), lineColor = options
-      .get('lineColor'), fillColor = options.get('fillColor');
-
-  var width = radius * 2 + lineWidth * 2;
-  var height = radius * 2 + lineWidth * 2;
-  var ratio = options.ratio;
-
-  return Stage.canvas(function(ctx) {
-    this.size(width, height, ratio);
-
-    ctx.scale(ratio, ratio);
-    ctx.beginPath();
-    ctx.arc(width / 2, height / 2, radius, 0, 2 * Math.PI);
-    if (fillColor) {
-      ctx.fillStyle = fillColor;
-      ctx.fill();
+    if (!verts.length) {
+      return;
     }
 
-    ctx.lineWidth = lineWidth;
-    ctx.strokeStyle = lineColor;
-    ctx.stroke();
-  });
-};
+    var width = 0, height = 0;
+    var ratio = options.ratio;
 
-Stage.SAT.prototype.drawConvex = function(verts, options) {
-  options = this.options.extend(options);
-  var lineWidth = options.get('lineWidth'), lineColor = options
-      .get('lineColor'), fillColor = options.get('fillColor');
-
-  if (!verts.length) {
-    return;
-  }
-
-  var width = 0, height = 0;
-  var ratio = options.ratio;
-
-  for (var i = 0; i < verts.length; i++) {
-    var v = verts[i], x = v.x, y = v.y;
-    width = Math.max(Math.abs(x), width);
-    height = Math.max(Math.abs(y), height);
-  }
-
-  return Stage.canvas(function(ctx) {
-    this.size(2 * width + 2 * lineWidth, 2 * height + 2 * lineWidth, ratio);
-
-    ctx.scale(ratio, ratio);
-    ctx.beginPath();
     for (var i = 0; i < verts.length; i++) {
-      var v = verts[i], x = v.x + width + lineWidth, y = v.y + height
-          + lineWidth;
-      if (i == 0)
-        ctx.moveTo(x, y);
-      else
-        ctx.lineTo(x, y);
+      var v = verts[i], x = v.x, y = v.y;
+      width = Math.max(Math.abs(x), width);
+      height = Math.max(Math.abs(y), height);
     }
 
-    if (verts.length > 2) {
-      ctx.closePath();
-    }
+    return Stage.canvas(function(ctx) {
+      this.size(2 * width + 2 * lineWidth, 2 * height + 2 * lineWidth, ratio);
 
-    if (fillColor) {
-      ctx.fillStyle = fillColor;
-      ctx.fill();
-      ctx.closePath();
-    }
+      ctx.scale(ratio, ratio);
+      ctx.beginPath();
+      for (var i = 0; i < verts.length; i++) {
+        var v = verts[i], x = v.x + width + lineWidth, y = v.y + height
+            + lineWidth;
+        if (i == 0)
+          ctx.moveTo(x, y);
+        else
+          ctx.lineTo(x, y);
+      }
 
-    ctx.lineWidth = lineWidth;
-    ctx.lineCap = "round";
-    ctx.strokeStyle = lineColor;
-    ctx.stroke();
-  });
-};
+      if (verts.length > 2) {
+        ctx.closePath();
+      }
+
+      if (fillColor) {
+        ctx.fillStyle = fillColor;
+        ctx.fill();
+        ctx.closePath();
+      }
+
+      ctx.lineWidth = lineWidth;
+      ctx.lineCap = "round";
+      ctx.strokeStyle = lineColor;
+      ctx.stroke();
+    });
+  };
+
+  if (typeof module !== 'undefined') {
+    module.exports = Viewer;
+  } else if (typeof define === 'function' && define.amd) {
+    define([], function() {
+      return Viewer;
+    });
+  } else if (typeof Stage !== 'undefined') {
+    Stage.SAT = Viewer;
+  } else if (typeof window !== 'undefined') {
+  } else if (typeof global !== 'undefined') {
+  } else if (typeof self !== 'undefined') {
+  } else {
+  }
+
+}).call(this);
