00f54ea24c90a580a77c903926bf56fda5f6780b
Ali Shakiba <shakiba@users.noreply.github.com>
2024-08-09 22:54:14 +0330

    build v1.0-alpha.4

--- dist/stage.js
+++ dist/stage.js
@@ -1,5 +1,5 @@
 /**
- * Stage.js 1.0.0-alpha.3
+ * Stage.js 1.0.0-alpha.4
  *
  * @copyright Copyright (c) 2024 Ali Shakiba
  * @license The MIT License (MIT)
--- dist/stage.umd.cjs
+++ dist/stage.umd.cjs
@@ -2,7 +2,7 @@
   typeof exports === "object" && typeof module !== "undefined" ? factory(exports) : typeof define === "function" && define.amd ? define(["exports"], factory) : (global = typeof globalThis !== "undefined" ? globalThis : global || self, factory(global.Stage = {}));
 })(this, function(exports2) {
   "use strict";/**
- * Stage.js 1.0.0-alpha.3
+ * Stage.js 1.0.0-alpha.4
  *
  * @copyright Copyright (c) 2024 Ali Shakiba
  * @license The MIT License (MIT)
